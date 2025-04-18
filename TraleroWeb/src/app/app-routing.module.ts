a82c8d6525da4b6a8c539e57ae065d39519c125f
PatricioFigueroa <127542058+PatricioFigueroa@users.noreply.github.com>
2025-04-18 18:19:48 -0400

    Creación de componentes y páginas (con errores)

--- TraleroWeb/src/app/app-routing.module.ts
+++ TraleroWeb/src/app/app-routing.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { BuscarProductoPage } from './pages/buscar-producto/buscar-producto.page';
+import { CategoriasPage } from './pages/categorias/categorias.page';
+import { TiendasPage } from './pages/tiendas/tiendas.page';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.page').then( m => m.HomePage)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'buscar-producto',
+    loadChildren: () => import('./pages/buscar-producto/buscar-producto.module').then( m => m.BuscarProductoPageModule)
+  },
+  {
+    path: 'categorias',
+    loadChildren: () => import('./pages/categorias/categorias.module').then( m => m.CategoriasPageModule)
+  },
+  {
+    path: 'tiendas',
+    loadChildren: () => import('./pages/tiendas/tiendas.module').then( m => m.TiendasPageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ TraleroWeb/src/app/pages/buscar-producto/buscar-producto.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-producto',
+  templateUrl: './buscar-producto.page.html',
+  styleUrls: ['./buscar-producto.page.scss'],
+})
+export class BuscarProductoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

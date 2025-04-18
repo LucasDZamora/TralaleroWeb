5a96892361cb6ba754551897a506a9c1cb0936ae
PatricioFigueroa <127542058+PatricioFigueroa@users.noreply.github.com>
2025-04-18 18:49:40 -0400

    Creacion header y linkeos a otras paginas

--- TraleroWeb/src/app/components/header/header.component.ts
+++ TraleroWeb/src/app/components/header/header.component.ts
@@ -4,12 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone: false
+standalone: false
 })
 export class HeaderComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+  openMenu() {
+    
+  }
 
 }

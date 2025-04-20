be2845ef879ca8b9b860358edbaf308b1df3bb85
Lucas Zamora <40323453+LucasDZamora@users.noreply.github.com>
2025-04-20 17:09:29 -0400

    FIltros

--- TraleroWeb/src/app/pages/buscar-producto/buscar-producto.page.ts
+++ TraleroWeb/src/app/pages/buscar-producto/buscar-producto.page.ts
@@ -5,26 +5,32 @@ import { Producto } from 'src/app/models/producto';
   selector: 'app-buscar-producto',
   templateUrl: './buscar-producto.page.html',
   styleUrls: ['./buscar-producto.page.scss'],
-  standalone: false,
+  standalone:false
 })
 export class BuscarProductoPage {
   productos: Producto[] = [
-    { id: 1, nombre: 'Laptop', descripcion: 'Gaming', precio: 1200, categoria: 'Tecnología', },
-    { id: 2, nombre: 'Zapatillas', descripcion: 'Deportivas', precio: 80, categoria: 'Ropa',},
-    { id: 3, nombre: 'Celular', descripcion: 'Smartphone', precio: 500, categoria: 'Tecnología'},
+    { id: 1, nombre: 'Laptop', descripcion: 'Gaming', precio: 1200, categoria: 'Tecnología' },
+    { id: 2, nombre: 'Zapatillas', descripcion: 'Deportivas', precio: 2000, categoria: 'Ropa' },
+    { id: 3, nombre: 'Celular', descripcion: 'Smartphone', precio: 2500, categoria: 'Tecnología' },
+    { id: 4, nombre: 'Polera', descripcion: 'Verano', precio: 1500, categoria: 'Ropa' },
+    { id: 5, nombre: 'Audífonos', descripcion: 'Bluetooth', precio: 3000, categoria: 'Tecnología' },
   ];
 
   productosFiltrados: Producto[] = [];
 
-  filtro = {
-    precioMin: 0,
-    precioMax: Infinity,
-    categoria: ''
-  };
-
   orden: 'asc' | 'desc' = 'asc';
 
+  filtro = {
+    categoria: '',
+  };
+
   categorias: string[] = [];
+  rangosPrecio = [
+    { min: 0, max: 1000, seleccionado: false },
+    { min: 1000, max: 2000, seleccionado: false },
+    { min: 2000, max: 3000, seleccionado: false },
+    { min: 3000, max: 4000, seleccionado: false },
+  ];
 
   ngOnInit() {
     this.productosFiltrados = [...this.productos];
@@ -38,11 +44,18 @@ export class BuscarProductoPage {
   }
 
   filtrar() {
+    const rangosSeleccionados = this.rangosPrecio.filter(r => r.seleccionado);
+
     this.productosFiltrados = this.productos.filter(p => {
-      const cumplePrecio = p.precio >= this.filtro.precioMin && p.precio <= this.filtro.precioMax;
       const cumpleCategoria = this.filtro.categoria ? p.categoria === this.filtro.categoria : true;
-      return cumplePrecio && cumpleCategoria;
+
+      const cumpleRango =
+        rangosSeleccionados.length === 0 ||
+        rangosSeleccionados.some(r => p.precio >= r.min && p.precio <= r.max);
+
+      return cumpleCategoria && cumpleRango;
     });
+
     this.ordenarProductos();
   }
 }
